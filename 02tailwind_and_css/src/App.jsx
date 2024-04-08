e3e9fb30435bdd994bbe3dfe7a1dcda6ff537307
Md Sanaullah <sanaullahcse141@gmail.com>
2024-04-09 02:01:31 +0530

    Day 02

---
+++ 02tailwind_and_css/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Cards from './Components/Cards'
+
+function App() {
+
+  return (
+    <>
+     <Cards utility='buy jackets'/>
+     <Cards/>
+    </>
+  )
+}
+
+export default App
